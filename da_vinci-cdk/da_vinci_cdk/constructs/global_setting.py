b75617f5e1679aa6aad7694e5f1e2428a8aa0c3b
Jim Rosser <jarosser06@gmail.com>
2025-05-05 14:05:26 -0700

    Add support for side car application

--- da_vinci-cdk/da_vinci_cdk/constructs/global_setting.py
+++ da_vinci-cdk/da_vinci_cdk/constructs/global_setting.py
@@ -2,13 +2,20 @@ from typing import Optional, Union
 
 from constructs import Construct
 
+from aws_cdk.custom_resources import (
+    AwsCustomResource,
+    AwsCustomResourcePolicy,
+    AwsSdkCall,
+    PhysicalResourceId,
+)
+
 from da_vinci.core.exceptions import GlobalSettingsNotEnabledError
 from da_vinci.core.tables.global_settings import (
     GlobalSetting as GlobalSettingTblObj,
     GlobalSettingType,
 )
 
-from da_vinci_cdk.constructs.base import custom_type_name
+from da_vinci_cdk.constructs.base import custom_type_name, resource_namer
 from da_vinci_cdk.constructs.dynamodb import DynamoDBItem
 
 
@@ -64,3 +71,76 @@ class GlobalSetting(DynamoDBItem):
             setting_value=setting.setting_value,
             scope=scope,
         )
+
+
+class GlobalSettingLookup(Construct):
+    """
+    Lookup construct for a global setting item.
+    """
+    def __init__(self, scope: Construct, construct_id: str, namespace: str, setting_key: str):
+        """
+        Initialize the global setting lookup construct.
+
+        Keyword Arguments:
+        scope: The CDK scope.
+        construct_id: The construct ID.
+        namespace: The namespace of the setting.
+        setting_key: The setting key.
+        """
+        super().__init__(scope, construct_id)
+
+        self.full_table_name = resource_namer(name=GlobalSettingTblObj.table_name, scope=self)
+
+        self.namespace = namespace
+
+        self.setting_key = setting_key
+
+        # Generate the DynamoDB key
+        self.item_key = GlobalSettingTblObj.gen_dynamodb_key(
+            partition_key_value=namespace,
+            sort_key_value=setting_key,
+        )
+
+        # Create a custom resource to look up the value
+        self.lookup_resource = AwsCustomResource(
+            scope=self,
+            id=f'{construct_id}-lookup',
+            policy=AwsCustomResourcePolicy.from_sdk_calls(
+                resources=[
+                    f'arn:aws:dynamodb:*:*:table/{self.full_table_name}',
+                    f'arn:aws:dynamodb:*:*:table/{self.full_table_name}/*',
+                ]
+            ),
+            on_create=self._get_item_call(),
+            on_update=self._get_item_call(),
+            resource_type='Custom::DynamoDBLookup',
+        )
+
+    def _get_item_call(self) -> AwsSdkCall:
+        """
+        Create a GetItem AWS SDK call for the resource lookup
+        
+        Returns:
+            AwsSdkCall: The GetItem call configuration
+        """
+        return AwsSdkCall(
+            action='getItem',
+            service='DynamoDB',
+            parameters={
+                'Key': self.item_key,
+                'TableName': self.full_table_name,
+                'ConsistentRead': True
+            },
+            physical_resource_id=PhysicalResourceId.of(
+                f'{self.namespace}-{self.setting_key}-lookup'
+            )
+        )
+    
+    def get_value(self) -> str:
+        """
+        Get the endpoint from the lookup result
+        
+        Returns:
+            str: The endpoint of the discovered resource
+        """
+        return self.lookup_resource.get_response_field(f'Item.SettingValue.S')
